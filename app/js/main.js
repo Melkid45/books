d8ec3faa81954c629e0d1d49f52463873537dbb7
Melkid69 <167970274+Melkid45@users.noreply.github.com>
2025-05-01 20:48:38 +0600

    last

---
+++ app/js/main.js
@@ -0,0 +1,21 @@
+function onEntry(entry) {
+    entry.forEach(change => {
+    if (change.isIntersecting) {
+    change.target.classList.add('element-show');
+    }
+    });
+}
+
+let options = {threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+
+for (let elm of elements) {
+    observer.observe(elm);
+}
+
+
+$('.open__menu').on('click', function(e){
+    $('.header__list').toggleClass('open')
+    $(this).toggleClass('open')
+})
