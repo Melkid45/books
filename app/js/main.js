347be698b1293babf3dccfa2c4b2800c96841ff2
Melkid69 <167970274+Melkid45@users.noreply.github.com>
2025-05-31 13:19:53 +0600

    last

--- app/js/main.js
+++ app/js/main.js
@@ -19,3 +19,29 @@ $('.open__menu').on('click', function(e){
     $('.header__list').toggleClass('open')
     $(this).toggleClass('open')
 })
+
+$('.number').on('mouseover', function(e){
+    let number = $(this).attr('data-modal')
+    $('.wrap').each(function(e){
+        if ($(this).attr('data-open') == number){
+            $(this).toggleClass('active')
+        }
+    })
+})
+
+// let width = $(window).width()
+
+// if (width >= 780){
+//     $('.dope ul li').each(function(e){
+//         $(this).attr('id', '')
+//     })
+// }
+
+
+
+var splide = new Splide( '#slide', {
+    perPage: 1,
+    type: 'loop'
+  } );
+  
+  splide.mount();
